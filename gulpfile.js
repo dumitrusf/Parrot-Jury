c065ceb5d21dc8226513b39a95e38e394affdc30
Stefan-Fernando <dumitrulovelace@gmail.com>
2024-06-14 23:19:03 +0200

    build(PJ-97): :building_construction: :wrench: installed and move project to apache htdocs #time 20m #comment next instal composer #assign dumitrulovelace@gmail.com #done

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 // importa la fuente(src) y el destino (dist) desde el script del package.json gulp
-import {src, dest, watch} from 'gulp'
-
+import {src, dest, watch, series} from 'gulp';
+//  parallel en vez de series para correr todo
 
 // tráeme toda la dependencia de sass pero ese paquete que traeremos se llamara dartSass
-import * as dartSass from 'sass' 
+import * as dartSass from 'sass'; 
 
 // traeme la dependencia de gulp-sass (es decir extraemos tambien gulpSass y ...)
-import gulpSass from 'gulp-sass'
+import gulpSass from 'gulp-sass';
 
 
 // ... y lo que hacemos es unirlas en una sola
 // voy a compilar sass usando las dependecia de sass(dartSass pero a la vez tambien gulpsass) 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
+
+export function js( done ) {
+
+    src('src/js/app.js')
+
+    .pipe( dest('dist/js'))
+    
+    done();
+};
+
 
 
 export function css( done ) {
@@ -19,13 +29,19 @@ export function css( done ) {
     src('src/scss/app.scss')
 
     // compilamos usando sass del package.json
-    .pipe( sass())
+    .pipe( sass().on('error', sass.logError))
     .pipe( dest('dist/css'))
     
 
-    done()
+    done();
 };
 
 export function dev() {
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
 };
+/* 
+// arrancar todas de golpe
+export default parallel(js, css, dev)
+ */
+// arrancar una y la acaba... arrancar una y la acaba...
+export default series(js, css, dev)
